815a069e42432acbf7436e469be0b8d00be740db
manavmc118-code <manavmc118@gmail.com>
2026-08-08 19:57:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const http = require("http");
 const fs = require("fs");
 
-// Railway આપોઆપ PORT સેટ કરશે, ન મળે તો જ 30021 લેશે
-const PORT = process.env.PORT || 30021;
+const PORT = process.env.PORT || 32003;
 
 const Server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("server started");
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  res.end("server is running....");
 });
 
-// "0.0.0.0" લખવું જરૂરી છે જેથી Railway બહારથી આવતી Request સાંભળી શકે
-Server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server started on port ${PORT}`);
+Server.listen(PORT, "0.0.0.0", ()=> {
+  console.log(`Server started`)
 });
