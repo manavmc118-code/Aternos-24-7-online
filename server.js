707fbb4f6f35d79cbb60956b0d44563a2fbbfcd8
manavmc118-code <manavmc118@gmail.com>
2026-08-08 19:15:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const http = require("http");
 const fs = require("fs");
 
+// Railway પોતે PORT પર્યાવરણ ચલ (Environment Variable) આપશે
+const PORT = process.env.PORT || 30021;
+
 const Server = http.createServer((req, res) => {
-  console.log("server running on port 09191");
   res.end("server started");
 });
 
-Server.listen(09192, ()=> {
-  console.log("Server started")
+Server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started on port ${PORT}`);
 });
